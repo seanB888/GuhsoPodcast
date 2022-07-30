2cbdb288ca9290d2cc97da4ce9958c817db8aad8
Sean Blake <sean.blake97@outlook.com>
2022-07-29 21:38:47 -0400

    added app icons

---
+++ GuhsoPodcast/Components/Cards/RecentShowCard.swift
@@ -0,0 +1,20 @@
+//
+//  RecentShowCard.swift
+//  GuhsoPodcast
+//
+//  Created by SEAN BLAKE on 7/29/22.
+//
+
+import SwiftUI
+
+struct RecentShowCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecentShowCard_Previews: PreviewProvider {
+    static var previews: some View {
+        RecentShowCard()
+    }
+}
---
+++ GuhsoPodcast/Components/Cards/SmallCard.swift
@@ -0,0 +1,20 @@
+//
+//  SmallCard.swift
+//  GuhsoPodcast
+//
+//  Created by SEAN BLAKE on 7/29/22.
+//
+
+import SwiftUI
+
+struct SmallCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SmallCard_Previews: PreviewProvider {
+    static var previews: some View {
+        SmallCard()
+    }
+}
